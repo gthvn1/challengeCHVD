74968b7712af7ed60aa4cbd809d9abea5713f371
gthouvenin <guillaume.thouvenin@polymtl.ca>
2011-12-10 22:10:34 +0100

    Ajout d'une fonction en JS pour valider le vol avant de l'envoyer au serveur. Pour l'instant cette fonction ne fait qu'afficher les informations du vol dans la zone de status.

--- index.php
+++ index.php
@@ -4,6 +4,37 @@
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
     <title>Challenge VolRando 2012 CHVD</title>
     <link rel="stylesheet" href="challenge.css">
+
+    <script type="text/javascript"> 
+    function checkVolrando()
+    {
+        var x = document.getElementById("saisieVolrando"),
+            monTexte = "  ===== INFO DU VOL ===== <br />";
+
+        // Il y a 7 elements dans cet ordre:
+        //  0) sommet
+        //  1) pilote
+        //  2) date
+        //  3) biplace
+        //  4) co2
+        //  5) commentaire
+        //  6) soumettre le volrando
+        if (x.length != 7) {
+            alert('Fatal Error');
+            return false;
+        }
+
+        monTexte = monTexte +
+                   x.elements[0].name + ' : ' + x.elements[0].selectedIndex + '<br />' +
+                   x.elements[1].name + ' : ' + x.elements[1].selectedIndex + '<br />' +
+                   x.elements[2].name + ' : ' + x.elements[2].value + '<br />' +
+                   x.elements[3].name + ' : ' + x.elements[3].checked + '<br />' +
+                   x.elements[4].name + ' : ' + x.elements[4].checked + '<br />' +
+                   x.elements[5].name + ' : ' + x.elements[5].value + '<br />';
+        //alert(monTexte);
+        document.getElementById('status').innerHTML= monTexte;
+    }
+    </script>
 </head>
 <body>
 
@@ -91,15 +122,17 @@ Note CSS: Les zone de saisie et de status font parties de la zone
     <div id="saisie">
       <h2> Zone de saisie des VolRandos </h2>
 
-      <form Method="POST" Action="index.php">
+      <!--
+      <form name="saisieVolrando" method="POST" action="index.php">
+      -->
+      <form id="saisieVolrando">
         <table id="invisible">
         <tr> <!-- Choix du sommet -->
           <td id="invisible"> Sommets </td>
           <td id="invisible">
             <select name="sommet">
-            <option value="">Choisir un sommet dans la liste</option>
             <option value="">-------------------------</option>
-            <option value="0">ou declarer un nouveau sommet</option>
+            <option value="0">Declarer un nouveau sommet</option>
             <option value="">-------------------------</option>
             <option value="1">Chartreuse - Dent de Crolles</option>
             <option value="2">Chartreuse - La Grande Sure</option>
@@ -113,9 +146,8 @@ Note CSS: Les zone de saisie et de status font parties de la zone
           <td id="invisible"> Pilote </td>   
           <td id="invisible">        
             <select name="pilote">
-            <option value="">Votre nom</option>
             <option value="">-------------------------</option>
-            <option value="O">ou ajoutez un nouveau pilote</option>
+            <option value="O">Ajoutez un nouveau pilote</option>
             <option value="">-------------------------</option>
             <option value="1">Guillaume</option>
             <option value="2">Laurence</option>
@@ -123,46 +155,31 @@ Note CSS: Les zone de saisie et de status font parties de la zone
           </td>
         </tr>
 
-        <tr> <!-- Choix de la date --> 
+        <!-- Choix de la date --> 
+        <tr>
           <td id="invisible"> Date (JJ/MM/YY) </td>
-          <td id="invisible"> <input type=text name=date  > </td>
+          <td id="invisible"> <input type="text" name="dateVol" id="dateVol"> </td>
         </tr>
         </table>
 
-        <input type="checkbox" name="biplace" value="biplace" />
+        <input type="checkbox" name="biplace" />
         Vol effectué en biplace (1 point de bonus)<br />
        
-        <input type="checkbox" name="mobilitedouce" value="mobilitedouce" />
+        <input type="checkbox" name="mobilitedouce" />
         Vol effectué en mobilité dite douce (c'est à titre indicatif)<br />
 
         Un commentaire sur le vol (sur une seule ligne)
         <input type="text" size=50 name="commentaire" />
         <br />
   
-        <br />
-        <input type=submit value="Soumettre votre volrando">
+        <input type="button" value="Soumettre votre volrando" onclick="checkVolrando()">
       </form>
     </div> <!-- Fin de saisie -->
 
-    <div id="status">
-      <h2> Console </h2>
-      <?php
-        $sommet        = $_POST['sommet'];
-        $date          = $_POST['date'];
-        $pilote        = $_POST['pilote'];
-        $biplace       = $_POST['biplace'];
-        $mobilitedouce = $_POST['mobilitedouce'];
-        $commentaire   = $_POST['commentaire'];
-        
-        echo 'sommet        : ', $sommet, '<br />';
-        echo 'date          : ', $date, '<br />';
-        echo 'pilote        : ', $pilote, '<br />';
-        echo 'biplace       : ', $biplace, '<br />';
-        echo 'mobilitedouce : ', $mobilitedouce, '<br />';
-        echo 'commentaire   : ', $commentaire, '<br />';
-      ?>
-      </p>
-    </div> <!-- Fin de status -->
+    <h2> Console </h2>
+      <div id="status">
+       En attente de la déclaration d'un vol... 
+      </div> <!-- Fin de status -->
 
   </div> <!-- Fin de gauche -->
 
