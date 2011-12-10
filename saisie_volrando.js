f7deeaade8f6fa74ceab1ebad0c2717bff655430
gthouvenin <guillaume.thouvenin@polymtl.ca>
2011-12-10 22:11:55 +0100

    Removed saisie_volrando.js parce que l'inclusion ne fonctionne pas :(

--- saisie_volrando.js
+++
@@ -1,9 +0,0 @@
-function maFonction(Id, nom) {
-    var xhr; // objet XmlHttpRequest
-
-    if (window.XMLHttpRequest)    //  Objet standard
-        xhr = new XMLHttpRequest();     //  Firefox, Safari, ...
-    else if (window.ActiveXObject)      //  Internet Explorer
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-}
